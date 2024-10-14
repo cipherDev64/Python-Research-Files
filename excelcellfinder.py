33ab73b31121148ec146ee43473d4f295abd9c6b
Atulya Manikandan <atulyamanikandan@gmail.com>
2024-10-14 15:48:56 +0530

    Create excelcellfinder.py

---
+++ excelcellfinder.py
@@ -0,0 +1,14 @@
+import openpyxl
+
+# Load the Excel workbook
+workbookfile = input("Enter the path of your file: ")
+workbook = openpyxl.load_workbook(workbookfile)
+
+# Select the active sheet
+sheet = workbook.active
+
+# Get the value of a specific cell
+cell = input("Enter the cell number: ")
+cell_value = sheet[cell].value
+
+print(cell_value)
